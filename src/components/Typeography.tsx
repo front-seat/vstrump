10fc1f946142f1716a19f4b7a79aed2bcb48d0ec
Dave <davepeck@gmail.com>
2024-01-19 13:19:31 -0800

    Adjust weights

--- src/components/Typeography.tsx
+++ src/components/Typeography.tsx
@@ -2,7 +2,7 @@ import clsx from "clsx";
 
 /** The primary headline font and metrics. */
 export const PLEIN_HEADLINE =
-  "font-plein font-bold text-[90px] leading-[86.4px] lg:text-[190px] lg:leading-[171px]";
+  "font-plein font-normal text-[90px] leading-[86.4px] lg:text-[190px] lg:leading-[171px]";
 
 export type TypographicComponent = React.FC<
   React.PropsWithChildren<{ className?: string }>
