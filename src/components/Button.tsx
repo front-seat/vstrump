bae2e72e56da07822308bbb70f9452a21e1f9dbe
Dave <davepeck@gmail.com>
2024-02-06 10:10:23 -0800

    Let's win

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -12,7 +12,7 @@ const Button: React.FC<Props> = ({ title, invert, className, onClick }) => (
     className={clsx(
       "cursor-pointer font-switzer font-semibold text-[20px] leading-[20px] border-2 px-[19px] py-[14px] transition-colors duration-200",
       invert
-        ? "border-darkest bg-sun text-darkest hover:text-white hover:border-white"
+        ? "border-darkest bg-sun text-darkest hover:text-medium hover:border-medium"
         : "border-sun bg-transparent text-sun hover:text-inherit hover:bg-sun",
       className
     )}
