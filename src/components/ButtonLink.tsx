dc2765a0d596216806ba0d4a624970b6b0352cf6
Dave <davepeck@gmail.com>
2024-02-06 13:40:27 -0800

    Center donate text

--- src/components/ButtonLink.tsx
+++ src/components/ButtonLink.tsx
@@ -17,7 +17,7 @@ export const ButtonLink: React.FC<Props> = ({
     href={href}
     target="_blank"
     className={clsx(
-      "block cursor-pointer font-switzer font-semibold text-[20px] leading-[20px] border-2 px-[19px] py-[14px] transition-colors duration-200",
+      "block cursor-pointer font-switzer font-semibold text-[20px] leading-[20px] border-2 px-[19px] py-[14px] transition-colors duration-200 text-center",
       invert
         ? "border-darkest bg-sun text-darkest hover:text-medium hover:border-medium"
         : "border-sun bg-transparent text-sun hover:text-inherit hover:bg-sun",
